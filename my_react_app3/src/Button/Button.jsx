c90210f41f49ccb00f1ff863f51b42919c29c033
Ramachandra <ramachandraudupa.sangamone@gmail.com>
2024-05-11 21:26:02 +0530

    update

--- my_react_app3/src/Button/Button.jsx
+++ my_react_app3/src/Button/Button.jsx
@@ -1,6 +1,19 @@
 import styles from './button.module.css'
 function Button()
 {
-    return(<button className={styles.button}>Click here</button>)
+    var style={
+            backgroundColor: "aqua",
+            color: "black",
+            padding: "10px 20px",
+            borderRadius: "5px",
+            border: "none",
+            cursor: "pointer",
+    }
+    return(
+        <div className="buttons">
+            <button className={styles.button}>Click here</button>
+            <button className={style}>Click</button>
+        </div>
+    )
 }
 export default Button
